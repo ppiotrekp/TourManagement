835a1db60a77c991d15a2d08a70c2f1012c44ded
Piotr Pyrczak <ppyrczak5@gmail.com>
2022-08-25 12:21:12 +0200

    Initial commit

---
+++ src/main/java/pl/ppyrczak/busschedulesystem/BusScheduleSystemApplication.java
@@ -0,0 +1,13 @@
+package pl.ppyrczak.busschedulesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusScheduleSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusScheduleSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/ppyrczak/busschedulesystem/BusScheduleSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.ppyrczak.busschedulesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusScheduleSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
