e5407a5dc859ab0223a8ec58818eeaee4f831b11
Piotr Pyrczak <ppyrczak5@gmail.com>
2022-08-25 12:27:53 +0200

    add: entity bus

--- src/main/java/pl/ppyrczak/busschedulesystem/model/Bus.java
+++ src/main/java/pl/ppyrczak/busschedulesystem/model/Bus.java
@@ -5,12 +5,19 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Getter
 @Setter
 @RequiredArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Bus {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String brand;
     private String model;
