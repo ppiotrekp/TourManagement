a3faddcf4ec6cb135294a2df1d5597f64b3cf287
Piotr Pyrczak <ppyrczak5@gmail.com>
2022-08-28 10:22:18 +0200

    method returns list created

--- src/main/java/pl/ppyrczak/busschedulesystem/controller/BusController.java
+++ src/main/java/pl/ppyrczak/busschedulesystem/controller/BusController.java
@@ -1,14 +1,25 @@
 package pl.ppyrczak.busschedulesystem.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.ppyrczak.busschedulesystem.model.Bus;
+import pl.ppyrczak.busschedulesystem.service.BusService;
+
+import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class BusController {
 
-    @GetMapping("/bus")
+    private final BusService busService;
+   /* @GetMapping("/bus")
     public Bus getBus() {
         return new Bus(1L, "Mercedes", "Tourismo", 20, "None");
+    }*/
+
+    @GetMapping("/buses")
+    public List<Bus> getBuses() {
+        return busService.getBuses();
     }
 }
