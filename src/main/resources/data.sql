ae558f18c952740df9da89c66c76d3a2376b53ce
Piotr Pyrczak <ppyrczak5@gmail.com>
2022-08-28 10:20:58 +0200

    add: some data into databse created

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into bus (brand, model, passengers_limit, equipment) values ('Mercedes', 'Tourismo', 20, 'toilet');
+insert into bus (brand, model, passengers_limit, equipment) values ('Mercedes', 'Tourismo 1', 10, 'toilet');
+insert into bus (brand, model, passengers_limit, equipment) values ('Mercedes', 'Tourismo 2', 40, 'toilet');
+insert into bus (brand, model, passengers_limit, equipment) values ('Mercedes', 'Tourismo 3', 110, 'toilet');
+insert into bus (brand, model, passengers_limit, equipment) values ('Mercedes', 'Tourismo 4', 20, 'toilet');
