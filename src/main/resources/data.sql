bcf4879741c83f284f4bcbe5e85870c9154f6fb0
Piotr Pyrczak <ppyrczak5@gmail.com>
2022-09-19 23:03:40 +0200

    new inserts for passenger table

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 insert into bus (brand, model, passengers_limit, equipment) values ('Mercedes', 'Tourismo', 20, 'toilet');
-insert into bus (brand, model, passengers_limit, equipment) values ('Mercedes', 'Tourismo 1', 10, 'toilet');
+insert into bus (brand, model, passengers_limit, equipment) values ('Mercedes', 'Tourismo 1', 5, 'toilet');
 insert into bus (brand, model, passengers_limit, equipment) values ('Mercedes', 'Tourismo 2', 40, 'toilet');
 insert into bus (brand, model, passengers_limit, equipment) values ('Mercedes', 'Tourismo 3', 110, 'toilet');
 insert into bus (brand, model, passengers_limit, equipment) values ('Mercedes', 'Tourismo 4', 20, 'toilet');
@@ -15,7 +15,18 @@ insert into bus (brand, model, passengers_limit, equipment) values ('Bmw', 'Tour
 insert into bus (brand, model, passengers_limit, equipment) values ('Audi', 'Tourismo 14', 20, 'toilet');
 
 insert into schedule (bus_id, departure_from, departure_to, departure, arrival, ticket_price ) values
-                    (1, 'Krakow', 'Gdansk', '2022-01-01 00:00:01','2022-01-02 00:00:01',100);
+                    (2, 'Krakow', 'Gdansk', '2022-01-01 00:00:01','2022-01-02 00:00:01',100);
 
 insert into schedule (bus_id, departure_from, departure_to, departure, arrival, ticket_price ) values
     (1, 'Krakow', 'Gdansk', '2022-01-01 00:00:01','2022-01-01 02:00:01',100);
+
+insert into passenger (schedule_id, first_name, last_name, email, phone_number) values
+    (1, 'Piotr', 'Pyrczak', 'ppyrczak1@gmail.com', '888111222');
+insert into passenger (schedule_id, first_name, last_name, email, phone_number) values
+    (1, 'Piotr', 'Pyrczak', 'ppyrczak2@gmail.com', '888111222');
+insert into passenger (schedule_id, first_name, last_name, email, phone_number) values
+    (1, 'Piotr', 'Pyrczak', 'ppyrczak3@gmail.com', '888111222');
+insert into passenger (schedule_id, first_name, last_name, email, phone_number) values
+    (1, 'Piotr', 'Pyrczak', 'ppyrczak4@gmail.com', '888111222');
+
+
