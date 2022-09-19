1aa6adb3a491190375fa2488a2f1a16e076f2b78
Piotr Pyrczak <ppyrczak5@gmail.com>
2022-09-19 17:23:59 +0200

    passenger entity created

---
+++ src/main/java/pl/ppyrczak/busschedulesystem/model/Passenger.java
@@ -0,0 +1,36 @@
+package pl.ppyrczak.busschedulesystem.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+@Getter
+@Setter
+@Entity
+public class Passenger {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank(message = "firstname is mandatory")
+    private String firstName;
+    @NotBlank(message = "lastname is mandatory")
+    private String lastName;
+    @NotBlank(message = "email is mandatory")
+    @Email(message = "email is not valid", regexp = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\" +
+            ".[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\" +
+            "x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\" +
+            ".)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\" +
+            ".){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f" +
+            "\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")
+    private String email;
+    @NotBlank(message = "phone number is mandatory")
+    @Pattern(message = "Invalid phone number", regexp = "^([1-9][0-9]{8})$")
+    private String phoneNumber;
+}
