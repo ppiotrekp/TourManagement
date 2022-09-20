8088fee56803592fdabeedbb83d4872944050f83
Piotr Pyrczak <ppyrczak5@gmail.com>
2022-09-20 16:18:12 +0200

    review repository created

---
+++ src/main/java/pl/ppyrczak/busschedulesystem/repository/ReviewRepository.java
@@ -0,0 +1,9 @@
+package pl.ppyrczak.busschedulesystem.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.ppyrczak.busschedulesystem.model.Review;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+}
