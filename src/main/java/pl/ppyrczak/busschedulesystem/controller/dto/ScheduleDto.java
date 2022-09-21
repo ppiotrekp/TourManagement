005b6d5508bf3ab0e6c67057274a7fc2389beae1
Piotr Pyrczak <ppyrczak5@gmail.com>
2022-09-21 12:30:47 +0200

    schedule dto created

--- src/main/java/pl/ppyrczak/busschedulesystem/controller/dto/ScheduleDto.java
+++ src/main/java/pl/ppyrczak/busschedulesystem/controller/dto/ScheduleDto.java
@@ -1,12 +1,9 @@
 package pl.ppyrczak.busschedulesystem.controller.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Getter
