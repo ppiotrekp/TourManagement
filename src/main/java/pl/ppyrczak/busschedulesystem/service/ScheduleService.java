7a2a68eb61d0e51a8942cb04ac30a03193a86d6a
Piotr Pyrczak <ppyrczak5@gmail.com>
2022-09-21 13:49:35 +0200

    getSchedulesWIthPassengersAndReviews method created

--- src/main/java/pl/ppyrczak/busschedulesystem/service/ScheduleService.java
+++ src/main/java/pl/ppyrczak/busschedulesystem/service/ScheduleService.java
@@ -2,15 +2,22 @@ package pl.ppyrczak.busschedulesystem.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.ppyrczak.busschedulesystem.model.Passenger;
+import pl.ppyrczak.busschedulesystem.model.Review;
 import pl.ppyrczak.busschedulesystem.model.Schedule;
+import pl.ppyrczak.busschedulesystem.repository.PassengerRepository;
+import pl.ppyrczak.busschedulesystem.repository.ReviewRepository;
 import pl.ppyrczak.busschedulesystem.repository.ScheduleRepository;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class ScheduleService {
     private final ScheduleRepository scheduleRepository;
+    private final PassengerRepository passengerRepository;
+    private final ReviewRepository reviewRepository;
 
     public List<Schedule> getSchedules() {
         return scheduleRepository.findAll();
@@ -42,5 +49,29 @@ public class ScheduleService {
 
     public void deleteSchedule(Long id) {
         scheduleRepository.deleteById(id);
+    }
+
+    public List<Schedule> getSchedulesWithPassengersAndReviews() {
+        List<Schedule> allSchedules = scheduleRepository.findAll();
+        List<Long> ids = allSchedules.stream()
+                .map(schedule -> schedule.getId())
+                .collect(Collectors.toList());
+        List<Passenger> passengers = passengerRepository.findAllByScheduleIdIn(ids);
+        List<Review> reviews = reviewRepository.findAllByScheduleIdIn(ids);
+        allSchedules.forEach(schedule -> schedule.setPassengers(extractPassengers(passengers, schedule.getId())));
+        allSchedules.forEach(schedule -> schedule.setReviews(extractReviews(reviews, schedule.getId())));
+        return allSchedules;
+    }
+
+    private List<Review> extractReviews(List<Review> reviews, Long id) {
+        return reviews.stream()
+                .filter(review -> review.getScheduleId() == id)
+                .collect(Collectors.toList());
+    }
+
+    private List<Passenger> extractPassengers(List<Passenger> passengers, Long id) {
+        return passengers.stream()
+                .filter(passenger -> passenger.getScheduleId() == (id))
+                .collect(Collectors.toList());
     }
 }
