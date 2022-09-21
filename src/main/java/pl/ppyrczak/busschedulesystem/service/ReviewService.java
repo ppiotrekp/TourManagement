fea2a875866527e54e641252772bde2574e8cde7
Piotr Pyrczak <ppyrczak5@gmail.com>
2022-09-21 17:19:06 +0200

    check if a review is after an arrival

--- src/main/java/pl/ppyrczak/busschedulesystem/service/ReviewService.java
+++ src/main/java/pl/ppyrczak/busschedulesystem/service/ReviewService.java
@@ -4,31 +4,36 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import pl.ppyrczak.busschedulesystem.model.Passenger;
 import pl.ppyrczak.busschedulesystem.model.Review;
+import pl.ppyrczak.busschedulesystem.model.Schedule;
 import pl.ppyrczak.busschedulesystem.repository.PassengerRepository;
 import pl.ppyrczak.busschedulesystem.repository.ReviewRepository;
+import pl.ppyrczak.busschedulesystem.repository.ScheduleRepository;
 
 @Service
 @RequiredArgsConstructor
 public class ReviewService {
     private final ReviewRepository reviewRepository;
     private final PassengerRepository passengerRepository;
+    private final ScheduleRepository scheduleRepository;
 
     public Review addReview(Review review) {
         Passenger passenger = passengerRepository.findById(review.getPassengerId()).orElseThrow();
-        System.out.println(review.getPassengerId());
+        Schedule schedule = scheduleRepository.findById(passenger.getScheduleId()).orElseThrow();
+        review.setCreated();
         if (reviewRepository.existsByPassengerId(review.getPassengerId()) ) {
             throw new RuntimeException("You have a review about this transit");
             //TODO wywalic wyjatek
         }
 
-        //else if ()
+        else if (review.getCreated().isBefore(schedule.getArrival())) {
+            throw new RuntimeException("You can not write a review before arrival");
+        }
 
         else if (passenger.getScheduleId() != review.getScheduleId()) {
             throw new RuntimeException("Bad schedule id");
         }
 
         else {
-            review.setCreated();
             return reviewRepository.save(review);
         }
 
