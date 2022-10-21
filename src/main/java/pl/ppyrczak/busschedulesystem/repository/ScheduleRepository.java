899b01895f4654937e5512bd4618e78bef0d6626
Piotr Pyrczak <ppyrczak5@gmail.com>
2022-10-21 19:23:25 +0200

    all methods in schedule repo already tested

--- src/main/java/pl/ppyrczak/busschedulesystem/repository/ScheduleRepository.java
+++ src/main/java/pl/ppyrczak/busschedulesystem/repository/ScheduleRepository.java
@@ -2,16 +2,12 @@ package pl.ppyrczak.busschedulesystem.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import pl.ppyrczak.busschedulesystem.model.Bus;
 import pl.ppyrczak.busschedulesystem.model.Schedule;
 
 import java.util.List;
 
 @Repository
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
-    List<Schedule> findAllByBusIdIn(List<Long> ids);
     List<Schedule> findAllByBusId(Long id);
-    boolean existsByBusId(Long id );
-
-    boolean existsByPassengers(Long id);
-
 }
---
+++ src/test/java/pl/ppyrczak/busschedulesystem/repository/ScheduleRepositoryTest.java
@@ -0,0 +1,61 @@
+package pl.ppyrczak.busschedulesystem.repository;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import pl.ppyrczak.busschedulesystem.model.Schedule;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class ScheduleRepositoryTest {
+
+    @Autowired
+    private ScheduleRepository scheduleRepository;
+
+    @Test
+    void shouldFindAllByBusId() {
+        Schedule schedule = new Schedule(1L,
+                "Krakow",
+                "Malaga",
+                LocalDateTime.of(2022, 10, 10, 10, 10),
+                LocalDateTime.of(2022, 10, 10, 12, 10),
+                "100",
+                null, null);
+
+        Schedule schedule1 = new Schedule(1L,
+                "Krakow",
+                "Malaga",
+                LocalDateTime.of(2022, 10, 10, 10, 10),
+                LocalDateTime.of(2022, 10, 10, 12, 10),
+                "100",
+                null, null);
+
+        Schedule schedule2 = new Schedule(1L,
+                "Krakow",
+                "Malaga",
+                LocalDateTime.of(2022, 10, 10, 10, 10),
+                LocalDateTime.of(2022, 10, 10, 12, 10),
+                "100",
+                null, null);
+
+        schedule.setId(1L);
+        schedule1.setId(2L);
+        schedule2.setId(3L);
+
+
+        scheduleRepository.save(schedule);
+        scheduleRepository.save(schedule1);
+        scheduleRepository.save(schedule2);
+
+        List<Schedule> schedules = scheduleRepository.findAllByBusId(1L);
+
+        Assertions.assertEquals(schedules.size(), 3);
+
+    }
+}
