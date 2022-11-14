f3ba032d6f823231ab006512b91fa6bc7136b709
Piotr Pyrczak <ppyrczak5@gmail.com>
2022-11-14 21:51:49 +0100

    new integration test to check getBus method from BusController

--- src/test/java/pl/ppyrczak/busschedulesystem/controller/BusControllerTest.java
+++ src/test/java/pl/ppyrczak/busschedulesystem/controller/BusControllerTest.java
@@ -2,12 +2,20 @@ package pl.ppyrczak.busschedulesystem.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.web.context.WebApplicationContext;
 import pl.ppyrczak.busschedulesystem.model.Bus;
 import pl.ppyrczak.busschedulesystem.repository.BusRepository;
 
@@ -20,8 +28,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+@WebAppConfiguration
+
+//@RunWith(SpringRunner.class)
+//@WebMvcTest(BusController.class)
 class BusControllerTest {
-/*
+
     @Autowired
     private MockMvc mockMvc;
     @Autowired
@@ -30,7 +44,7 @@ class BusControllerTest {
     private BusRepository busRepository;
 
     @Test
-    @Transactional
+    @WithMockUser(roles = {"ADMIN"})
     void shouldGetBus() throws Exception {
 
         Bus newBus = new Bus();
@@ -40,7 +54,7 @@ class BusControllerTest {
         newBus.setPassengersLimit(20);
         busRepository.save(newBus);
 
-        MvcResult mvcResult = mockMvc.perform(get("/bus/" + newBus.getId()))
+        MvcResult mvcResult = mockMvc.perform(get("/buses/" + newBus.getId()))
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(status().is(200))
                 .andReturn();
@@ -49,5 +63,5 @@ class BusControllerTest {
         assertThat(bus).isNotNull();
         assertThat(bus.getId()).isEqualTo(newBus.getId());
         assertThat(bus.getModel()).isEqualTo("V200");
-    }*/
+    }
 }
