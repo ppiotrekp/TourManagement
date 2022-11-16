635787032124ea0ab6721c315842524cb38b2800
Piotr Pyrczak <ppyrczak5@gmail.com>
2022-11-16 23:12:55 +0100

    changed class name - to separate responsibility of class to handle only admin requests

--- src/test/java/pl/ppyrczak/busschedulesystem/controller/BusControllerAdminTest.java
+++ src/test/java/pl/ppyrczak/busschedulesystem/controller/BusControllerAdminTest.java
@@ -1,28 +1,21 @@
 package pl.ppyrczak.busschedulesystem.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.After;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.web.context.WebApplicationContext;
 import pl.ppyrczak.busschedulesystem.model.Bus;
 import pl.ppyrczak.busschedulesystem.repository.BusRepository;
 
-import javax.transaction.Transactional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -36,7 +29,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration
 @WebAppConfiguration
-class BusControllerTest {
+@WithMockUser(roles = {"ADMIN"})
+class BusControllerAdminTest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -51,7 +45,6 @@ class BusControllerTest {
     }
 
     @Test
-    @WithMockUser(roles = {"ADMIN"})
     void shouldGetBus() throws Exception {
 
         Bus newBus = new Bus();
@@ -73,7 +66,6 @@ class BusControllerTest {
     }
 
     @Test
-    @WithMockUser(roles = {"ADMIN"})
     void shouldAddBus() throws Exception {
         Bus newBus = new Bus();
         newBus.setBrand("Opel");
@@ -90,7 +82,6 @@ class BusControllerTest {
     }
 
     @Test
-    @WithMockUser(roles = {"ADMIN"})
     void shouldDeleteBus() throws Exception {
         Bus newBus = new Bus();
         newBus.setBrand("Mercedes");
