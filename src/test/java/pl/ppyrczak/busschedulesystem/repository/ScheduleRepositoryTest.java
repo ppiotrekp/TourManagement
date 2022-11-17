51f3200c5d5960dafec6e45d2d7c0648a9f71259
Piotr Pyrczak <ppyrczak5@gmail.com>
2022-11-17 12:14:11 +0100

    test from ScheduleRepositoryTes improved

--- src/test/java/pl/ppyrczak/busschedulesystem/repository/ScheduleRepositoryTest.java
+++ src/test/java/pl/ppyrczak/busschedulesystem/repository/ScheduleRepositoryTest.java
@@ -24,9 +24,7 @@ class ScheduleRepositoryTest {
     @Test
     void shouldFindAllByBusId() {
         Bus bus = new Bus("A", "M", 10, "a", null);
-        bus.setId(1L);
         busRepository.save(bus);
-
 
         Schedule schedule = new Schedule(bus.getId(),
                 "Krakow",
@@ -35,32 +33,9 @@ class ScheduleRepositoryTest {
                 LocalDateTime.of(2022, 10, 10, 12, 10),
                 "100",
                 null, null);
-
-//        Schedule schedule1 = new Schedule(1L,
-//                "Krakow",
-//                "Malaga",
-//                LocalDateTime.of(2022, 10, 10, 10, 10),
-//                LocalDateTime.of(2022, 10, 10, 12, 10),
-//                "100",
-//                null, null);
-//
-//        Schedule schedule2 = new Schedule(1L,
-//                "Krakow",
-//                "Malaga",
-//                LocalDateTime.of(2022, 10, 10, 10, 10),
-//                LocalDateTime.of(2022, 10, 10, 12, 10),
-//                "100",
-//                null, null);
-
-        schedule.setId(1L);
-//        schedule1.setId(2L);
-//        schedule2.setId(3L);
-
         scheduleRepository.save(schedule);
-//        scheduleRepository.save(schedule1);
-//        scheduleRepository.save(schedule2);
 
-        List<Schedule> schedules = scheduleRepository.findAllByBusId(1L);
+        List<Schedule> schedules = scheduleRepository.findAllByBusId(bus.getId());
 
         Assertions.assertEquals(schedules.size(), 1);
     }
