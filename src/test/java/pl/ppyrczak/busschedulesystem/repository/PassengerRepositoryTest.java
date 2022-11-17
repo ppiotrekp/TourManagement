4dbcdfb84c5c46f3580fa61e6b245b21547f3d7b
Piotr Pyrczak <ppyrczak5@gmail.com>
2022-11-17 12:12:22 +0100

    tests from PassengerRepositoryTest improved

--- src/test/java/pl/ppyrczak/busschedulesystem/repository/PassengerRepositoryTest.java
+++ src/test/java/pl/ppyrczak/busschedulesystem/repository/PassengerRepositoryTest.java
@@ -1,15 +1,13 @@
 package pl.ppyrczak.busschedulesystem.repository;
 
-
 import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import pl.ppyrczak.busschedulesystem.auth.ApplicationUser;
+import org.springframework.test.context.junit4.SpringRunner;
 import pl.ppyrczak.busschedulesystem.model.Bus;
 import pl.ppyrczak.busschedulesystem.model.Passenger;
 import pl.ppyrczak.busschedulesystem.model.Schedule;
@@ -20,99 +18,62 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @DataJpaTest
-//@RunWith(SpringRunner.class)
+@RunWith(SpringRunner.class)
 class PassengerRepositoryTest {
     @Autowired
     private PassengerRepository passengerRepository;
     @Autowired
-    private UserRepository userRepository;
-    @Autowired
     private BusRepository busRepository;
-
     @Autowired
     private ScheduleRepository scheduleRepository;
 
-    @BeforeEach
-    void setUp() {
-        passengerRepository.deleteAll();
+    private Bus createBus() {
+        Bus bus = new Bus();
+        bus.setBrand("Mercedes");
+        bus.setModel("V200");
+        bus.setEquipment("kitchen");
+        bus.setPassengersLimit(20);
+        busRepository.save(bus);
+        return bus;
+    }
+
+    private Schedule createSchedule() {
+        Schedule schedule = new Schedule();
+        schedule.setBusId(createBus().getId());
+        schedule.setDepartureFrom("Krakow");
+        schedule.setDepartureTo("Malaga");
+        schedule.setDeparture(LocalDateTime.of(2023, 10, 10, 10, 10));
+        schedule.setArrival(LocalDateTime.of(2023, 10, 10, 12, 10));
+        schedule.setTicketPrice("100");
+        scheduleRepository.save(schedule);
+        return schedule;
     }
 
     @AfterEach
     void tearDown() {
         passengerRepository.deleteAll();
+        busRepository.deleteAll();
+        scheduleRepository.deleteAll();
     }
 
     @Test
     void shouldReturnTakenSeatsById() {
-        Bus bus = new Bus(1L,
-                "Merdeces",
-                "Vivaro",
-                10,
-                "toilet",
-                null);
-
-        busRepository.save(bus);
-
-        Schedule schedule = new Schedule(1L,
-                "Krakow",
-                "Malaga",
-                LocalDateTime.of(2022, 10, 10, 10, 10),
-                LocalDateTime.of(2022, 10, 10, 12, 10),
-                "100",
-                null, null);
-
-        scheduleRepository.save(schedule);
-
-        ApplicationUser user = new ApplicationUser("Piotr",
-                "Pyrczak",
-                "piotr@gmail.com",
-                "piotr",
-                null);
-
-        user.setId(1L);
-        userRepository.save(user);
-
-        Passenger passenger = new Passenger(1L, 1L, 1L, 1);
-        Passenger passenger1 = new Passenger(2L, 1L, 1L, 1);
+        Schedule schedule = createSchedule();
+        Passenger passenger = new Passenger(1L, 1L, schedule.getId(), 1);
+        Passenger passenger1 = new Passenger(2L, 1L, schedule.getId(), 2);
         passengerRepository.save(passenger);
         passengerRepository.save(passenger1);
 
-        int expected = passengerRepository.takenSeatsById(1L);
-        Assert.assertEquals(expected, 2);
+        int expected = passengerRepository.takenSeatsById(schedule.getId());
+        Assert.assertEquals(expected, 3);
     }
 
     @Test
     void shouldFindAllByScheduleIdInWorks() {
-        Bus bus = new Bus(1L,
-                "Merdeces",
-                "Vivaro",
-                10,
-                "toilet",
-                null);
+        Schedule schedule = createSchedule();
 
-        busRepository.save(bus);
-
-        Schedule schedule = new Schedule(1L,
-                "Krakow",
-                "Malaga",
-                LocalDateTime.of(2022, 10, 10, 10, 10),
-                LocalDateTime.of(2022, 10, 10, 12, 10),
-                "100",
-                null, null);
-
-        scheduleRepository.save(schedule);
-
-        ApplicationUser user = new ApplicationUser("Piotr",
-                "Pyrczak",
-                "piotr@gmail.com",
-                "piotr",
-                null);
-
-        user.setId(1L);
-        userRepository.save(user);
-
-        Passenger passenger = new Passenger(1L, 1L, 1L, 1);
-        Passenger passenger1 = new Passenger(2L, 1L, 1L, 1);
+        Passenger passenger = new Passenger(1L, schedule.getId(), 1);
+        Passenger passenger1 = new Passenger(1L, schedule.getId(), 1);
         passengerRepository.save(passenger);
         passengerRepository.save(passenger1);
 
@@ -128,14 +89,21 @@ class PassengerRepositoryTest {
 
     @Test
     void shouldFindAllByUserIdIn() {
+        Bus bus = new Bus(
+                "Merdeces",
+                "Vivaro",
+                10,
+                "toilet",
+                null);
+        busRepository.save(bus);
 
-        Passenger passenger = new Passenger(1L, 1L, 1L, 1);
-        Passenger passenger1 = new Passenger(2L, 1L, 2L, 1);
-
+        Schedule schedule = createSchedule();
+        Schedule schedule1 = createSchedule();
+        Passenger passenger = new Passenger(1L, schedule.getId(), 1);
+        Passenger passenger1 = new Passenger(1L, schedule1.getId(), 1);
         passengerRepository.save(passenger);
         passengerRepository.save(passenger1);
-
-        List<Long> ids = Arrays.asList(1L);
+        List<Long> ids = List.of(passenger.getUserId());
 
         List<Passenger> passengers = passengerRepository.findAllByUserIdIn(ids);
         System.out.println(passengers.toString());
@@ -144,15 +112,42 @@ class PassengerRepositoryTest {
 
     @Test
     void shouldFindByScheduleId() {
-        Passenger passenger = new Passenger(1L, 1L, 1L, 2);
-        Passenger passenger1 = new Passenger(2L, 2L, 1L, 2);
-        Passenger passenger2 = new Passenger(3L, 3L, 2L, 1);
+        Bus bus = new Bus(
+                "Merdeces",
+                "Vivaro",
+                10,
+                "toilet",
+                null);
+        busRepository.save(bus);
 
+        Schedule schedule = new Schedule(
+                bus.getId(),
+                "Krakow",
+                "Malaga",
+                LocalDateTime.of(2022, 10, 10, 10, 10),
+                LocalDateTime.of(2022, 10, 10, 12, 10),
+                "100",
+                null, null);
+        scheduleRepository.save(schedule);
+
+        Schedule schedule1 = new Schedule(
+                bus.getId(),
+                "Krakow",
+                "Malaga",
+                LocalDateTime.of(2022, 10, 10, 10, 10),
+                LocalDateTime.of(2022, 10, 10, 12, 10),
+                "100",
+                null, null);
+        scheduleRepository.save(schedule1);
+
+        Passenger passenger = new Passenger(1L, schedule.getId(), 2);
+        Passenger passenger1 = new Passenger(2L, schedule.getId(), 2);
+        Passenger passenger2 = new Passenger(3L, schedule1.getId(), 1);
         passengerRepository.save(passenger);
         passengerRepository.save(passenger1);
         passengerRepository.save(passenger2);
 
-        List<Passenger> passengersList = passengerRepository.findByScheduleId(2L);
+        List<Passenger> passengersList = passengerRepository.findByScheduleId(schedule1.getId());
         Assertions.assertEquals(passengersList.size(), 1);
     }
 }
