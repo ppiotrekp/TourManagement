560a8d14e1a7b0ed3e715617b32659953dcb939b
Piotr Pyrczak <ppyrczak5@gmail.com>
2022-11-22 14:49:32 +0100

    isBusAvailable method improved - now there is no posibility to add a new trip with the same time of arrival and departure

--- src/main/java/pl/ppyrczak/busschedulesystem/service/logic/Constraint.java
+++ src/main/java/pl/ppyrczak/busschedulesystem/service/logic/Constraint.java
@@ -13,7 +13,6 @@ import java.util.List;
 @Component
 @Slf4j
 public class Constraint {
-
     private final ScheduleRepository scheduleRepository;
 
     public Constraint(ScheduleRepository scheduleRepository) {
@@ -28,13 +27,18 @@ public class Constraint {
         List<Schedule> schedules = scheduleRepository.findAllByBusId(schedule.getBusId());
         boolean returnStat = true;
 
-        log.info("schedules: " + schedules.size());
         for (Schedule scheduleIterator : schedules) {
             if ((scheduleIterator.getArrival().isBefore(schedule.getDeparture()) &&
                     HOURS.between(scheduleIterator.getArrival(), schedule.getDeparture()) < 24L ) ||
 
                     (schedule.getArrival().isBefore(scheduleIterator.getDeparture()) &&
-                            HOURS.between(schedule.getArrival(), scheduleIterator.getDeparture()) < 24L )) {
+                            HOURS.between(schedule.getArrival(), scheduleIterator.getDeparture()) < 24L ) ||
+
+                    schedule.getArrival().isEqual(scheduleIterator.getArrival()) ||
+                    schedule.getArrival().isEqual(scheduleIterator.getDeparture()) ||
+                    schedule.getArrival().isEqual(scheduleIterator.getArrival()) ||
+                    schedule.getArrival().isEqual(scheduleIterator.getDeparture())
+            ) {
                 returnStat = false;
             }
         }
