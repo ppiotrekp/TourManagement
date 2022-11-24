c9aea38e3f42234eff6a4bdd3cebde121b708382
Piotr Pyrczak <ppyrczak5@gmail.com>
2022-11-24 11:03:19 +0100

    exception message corrected

--- src/main/java/pl/ppyrczak/busschedulesystem/exception/runtime/ReviewExistsException.java
+++ src/main/java/pl/ppyrczak/busschedulesystem/exception/runtime/ReviewExistsException.java
@@ -2,6 +2,6 @@ package pl.ppyrczak.busschedulesystem.exception.runtime;
 
 public class ReviewExistsException extends RuntimeException {
     public ReviewExistsException(Long id) {
-        super("Passenger with id: " + id + " has already review");
+        super("Passenger with id: " + id + " has already had review");
     }
 }
