c7c9bf95a29837392d42153ecd1fd815e60e26d6
Piotr Pyrczak <ppyrczak5@gmail.com>
2023-03-06 16:32:53 +0100

    header created

--- TourManagementFrontend/src/app/app.module.ts
+++ TourManagementFrontend/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BusComponent } from './components/bus/bus.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BusComponent
   ],
   imports: [
     BrowserModule
