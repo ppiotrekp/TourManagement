c95b2a2d01a84aa4331d89f5ba0ff78fa5a93f59
Piotr Pyrczak <ppyrczak5@gmail.com>
2023-03-06 19:10:36 +0100

    transit component created

---
+++ TourManagementFrontend/src/app/components/transit/transit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transit',
+  templateUrl: './transit.component.html',
+  styleUrls: ['./transit.component.css']
+})
+export class TransitComponent {
+
+}
