84101752d2abefce9b56ab114a3e9c7b5239cf1a
Piotr Pyrczak <ppyrczak5@gmail.com>
2023-03-06 16:52:47 +0100

    login service created

---
+++ TourManagementFrontend/src/app/service/login.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {StorageService} from "./storage.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  email = '';
+  password = '';
+  private header = new HttpHeaders();
+  private refreshToken: any;
+  loginUrl = 'http://localhost:8080/login';
+  refreshTokenUrl = 'http://localhost:8080/refresh-token'
+
+
+  constructor(private httpClient: HttpClient, private storageService: StorageService) { }
+
+  login(payload: any) {
+    return this.httpClient.post(this.loginUrl, payload);
+  }
+
+
+  setHeader() {
+    this.refreshToken = this.storageService.getEncodedToken().refreshToken;
+    this.header = this.header.set('Authorization', 'Bearer ' + this.refreshToken);
+    console.log(this.refreshToken);
+    console.log(this.header);
+  }
+
+  getRefreshToken() {
+    this.setHeader();
+    console.log(this.header)
+    return this.httpClient.post(this.refreshTokenUrl, {'headers': this.header});
+  }
+
+
+}
